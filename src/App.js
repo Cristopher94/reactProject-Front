e54def133189dc1a890a002ef7b5a65531e929fb
Cris Baez <cristopherbaeza@hotmail.com>
2022-03-03 11:20:19 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React  from 'react';
+import { Button } from 'semantic-ui-react';
+import { ApolloProvider } from "@apollo/client";
+import client from "./config/apollo";
+
+function App() {
+  return (
+
+    <ApolloProvider client={client}>
+    <div className="app">
+      <h1>Estamos en APP </h1>
+      <div>
+        <Button primary>Primary</Button>
+        <Button secondary>Secondary</Button>
+      </div>
+    </div>
+    </ApolloProvider>
+    
+  );
+}
+
+export default App;
